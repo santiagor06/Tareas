d374a178780b6166c5453cca263fb819a1c57259
santiagor06 <santiago.restrepo06@hotmail.com>
2024-02-12 11:58:40 -0500

    models

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +7,10 @@ namespace api.Models
 {
     public class Categoria
     {
+        [Key]
         public Guid CategoriaId{set;get;}
+        [Required]
+        [MaxLength(150)]
         public string Nombre{set;get;}
         public string Descripcion{set;get;}
         public virtual ICollection<Tarea>Tareas{set;get;}
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +9,18 @@ namespace api.Models
 {
     public class Tarea
     {
+        [Key]
         public Guid TareaId{set;get;}
+        [ForeignKey("CategoriaId")]
         public Guid CategoriaId{set;get;}
+        [Required]
+        [MaxLength(250)]
         public string Titulo{set;get;}
         public string Descripcion{set;get;}
         public Prioridad Prioridad{set;get;}
         public DateTime Fecha{set;get;}
+        [NotMapped]
+        public string Resumen{set;get;}
         public virtual Categoria Categoria{set;get;}
     }
 
